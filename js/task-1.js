633be284a0ef483a2b6dfef1eb83210ff6240991
Roman Koplyk <romasve94@gmail.com>
2026-04-20 21:25:10 +0200

    add changes

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categoriesItem = document.querySelectorAll(`#categories .item`)
-console.log(`Numbers of categories: ${categoriesItem.length}`)
-categoriesItem.forEach(item => {
-    const categName = item.querySelector(`h2`).textContent;
-    const elementsCount = item.querySelectorAll(`ul li`).length;
-    console.log(`Category: ${categName}`);
-    console.log(`Elements: ${elementsCount}`)
-})
